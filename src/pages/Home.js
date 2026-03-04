a0092d84982cb36817d67ac315364309c085d32f
NubricSolution <nubrixsolution@gmail.com>
2026-03-05 00:35:30 +0530

    UI layout + responsive services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar";
 import HeroSlider from "../components/HeroSlider";
 import Services from "../components/Services";
 import About from "../components/About";
-import Careers from "../components/Careers";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+// import Careers from "../components/Careers";
+// import Contact from "../components/Contact";
+// import Footer from "../components/Footer";
 
 function Home() {
   return (
@@ -14,9 +14,9 @@ function Home() {
       <HeroSlider />
       <Services />
       <About />
-      <Careers />
+      {/* <Careers />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
